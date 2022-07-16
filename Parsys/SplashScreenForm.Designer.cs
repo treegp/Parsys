411c73ce25cfc338c80bdad48ceaf0e57c531a0a
Saeed MJ <treegp@gmail.com>
2022-07-16 07:10:49 +0430

    Change UI

--- Parsys/SplashScreenForm.Designer.cs
+++ Parsys/SplashScreenForm.Designer.cs
@@ -41,9 +41,10 @@
             this.TitleLabel.BackColor = System.Drawing.Color.Transparent;
             this.TitleLabel.Font = new System.Drawing.Font("B Yekan", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
             this.TitleLabel.ForeColor = System.Drawing.SystemColors.InactiveCaption;
-            this.TitleLabel.Location = new System.Drawing.Point(388, 346);
+            this.TitleLabel.Location = new System.Drawing.Point(315, 289);
+            this.TitleLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.TitleLabel.Name = "TitleLabel";
-            this.TitleLabel.Size = new System.Drawing.Size(332, 45);
+            this.TitleLabel.Size = new System.Drawing.Size(264, 36);
             this.TitleLabel.TabIndex = 0;
             this.TitleLabel.Text = "نرم افزار انبارداری پارسیس";
             // 
@@ -52,10 +53,11 @@
             this.SplashScreenProgressBar.BackColor = System.Drawing.Color.Black;
             this.SplashScreenProgressBar.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.SplashScreenProgressBar.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
-            this.SplashScreenProgressBar.Location = new System.Drawing.Point(0, 427);
+            this.SplashScreenProgressBar.Location = new System.Drawing.Point(0, 347);
+            this.SplashScreenProgressBar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.SplashScreenProgressBar.Name = "SplashScreenProgressBar";
             this.SplashScreenProgressBar.RightToLeftLayout = true;
-            this.SplashScreenProgressBar.Size = new System.Drawing.Size(800, 23);
+            this.SplashScreenProgressBar.Size = new System.Drawing.Size(600, 19);
             this.SplashScreenProgressBar.Step = 500;
             this.SplashScreenProgressBar.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
             this.SplashScreenProgressBar.TabIndex = 1;
@@ -67,25 +69,27 @@
             this.ProgressLabel.BackColor = System.Drawing.Color.Transparent;
             this.ProgressLabel.Font = new System.Drawing.Font("B Yekan", 7F);
             this.ProgressLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ProgressLabel.Location = new System.Drawing.Point(39, 400);
+            this.ProgressLabel.Location = new System.Drawing.Point(29, 325);
+            this.ProgressLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.ProgressLabel.Name = "ProgressLabel";
-            this.ProgressLabel.Size = new System.Drawing.Size(729, 24);
+            this.ProgressLabel.Size = new System.Drawing.Size(547, 20);
             this.ProgressLabel.TabIndex = 0;
             this.ProgressLabel.Text = "در حال بارگزاری سیستم";
             this.ProgressLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // SplashScreenForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Parsys.Properties.Resources.keyboard_15;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.SplashScreenProgressBar);
             this.Controls.Add(this.ProgressLabel);
             this.Controls.Add(this.TitleLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "SplashScreenForm";

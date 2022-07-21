5764e06fa2dbceb061b666259693a68bffae8e5f
Saeed MJ <treegp@gmail.com>
2022-07-21 18:59:13 +0430

    Update ConnectSQL

--- Parsys.DataLayer/Connections/ProviderMethods/ConnectSQL.cs
+++ Parsys.DataLayer/Connections/ProviderMethods/ConnectSQL.cs
@@ -7,18 +7,19 @@ namespace Parsys.DataLayer.Connections.ProviderMethods
 {
     public class ConnectToSQL : IConnection
     {
-        SqlConnectionStringBuilder sqlCon = new SqlConnectionStringBuilder();
-        string dbConnectionName = "parsysdb";
+        private SqlConnectionStringBuilder sqlCon = new SqlConnectionStringBuilder();
+        private readonly SqlConnectionStringBuilder masterCon = new SqlConnectionStringBuilder();
+        public string DbConnectionName { get => "parsysdb"; }
 
-        public ConnectToSQL(string Connection)
+        public ConnectToSQL()
         {
-            sqlCon.ConnectionString = Connection;
+            GetConnection();
         }
+
+
 
         public async Task<bool> CheckConnection()
         {
-            SqlConnectionStringBuilder masterCon = sqlCon;
-            masterCon.InitialCatalog = "master";
             using (SqlConnection con = new SqlConnection(masterCon.ConnectionString))
             {
                 try
@@ -33,58 +34,67 @@ namespace Parsys.DataLayer.Connections.ProviderMethods
             }
         }
 
+
+
         public async Task<bool> CheckDatabase()
         {
-            SqlConnectionStringBuilder masterCon = sqlCon;
-            masterCon.InitialCatalog = "master";
             using (SqlConnection con = new SqlConnection(masterCon.ConnectionString))
             {
                 try
                 {
                     await con.OpenAsync();
-                    SqlCommand com = new SqlCommand("select count(*) from sys.databases where name= N'" + dbConnectionName + "'", con);
+                    SqlCommand com = new SqlCommand("select count(*) from sys.databases where name= N'" + DbConnectionName + "'", con);
                     if ((int)await com.ExecuteScalarAsync() == 1)
                         return true;
+                    return false;
                 }
-                catch { }
-                return false;
-
+                catch { return false; }
             }
         }
+
+
 
         public async Task<bool> CreateDatabase(string script)
         {
-            SqlConnectionStringBuilder masterCon = sqlCon;
-            masterCon.InitialCatalog = "master";
             using (SqlConnection con = new SqlConnection(masterCon.ConnectionString))
             {
                 try
                 {
                     await con.OpenAsync();
-                    SqlCommand com = new SqlCommand("create database " + dbConnectionName, con);
-                    if ((await com.ExecuteNonQueryAsync() == 1))
+                    SqlCommand com = new SqlCommand("create database " + DbConnectionName, con);
+                    await com.ExecuteNonQueryAsync();
+                    using (SqlConnection conScript = new SqlConnection(sqlCon.ConnectionString))
                     {
-                        using (SqlConnection conScript = new SqlConnection(sqlCon.ConnectionString))
-                        {
-                            await conScript.OpenAsync();
-                            string scriptText = Properties.Resources.script.Replace("[ShopDb]", "[" + dbConnectionName + "]");
-                            SqlCommand comScript = new SqlCommand(scriptText, con);
-                            if (await comScript.ExecuteNonQueryAsync() == 1)
-                                return true;
-                        }
+                        await conScript.OpenAsync();
+                        string scriptText = script.Replace("[ShopDb]", "[" + DbConnectionName + "]");
+                        SqlCommand comScript = new SqlCommand(scriptText, con);
+                        if (await comScript.ExecuteNonQueryAsync() == 1)
+                            return true;
                     }
+                    return false;
                 }
-                catch { }
-                return false;
+                catch { return false; }
             }
         }
 
 
-        public void RefreshConnection(string connection)
+
+        public string GetConnection()
+        {
+            ConfigurationManager.RefreshSection("connectionStrings");
+            sqlCon.ConnectionString = ConfigurationManager.ConnectionStrings["PARSYS"].ConnectionString;
+            masterCon.ConnectionString = sqlCon.ConnectionString;
+            masterCon.InitialCatalog = "master";
+            return sqlCon.ConnectionString;
+        }
+
+
+
+        public void SetConnection(string ConnectionsString)
         {
             var configManager = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-            configManager.ConnectionStrings.ConnectionStrings["PARSYS"].ConnectionString = connection;
-            configManager.Save(ConfigurationSaveMode.Modified,true);
+            configManager.ConnectionStrings.ConnectionStrings["PARSYS"].ConnectionString = ConnectionsString;
+            configManager.Save(ConfigurationSaveMode.Modified, true);
             ConfigurationManager.RefreshSection("connectionStrings");
         }
     }

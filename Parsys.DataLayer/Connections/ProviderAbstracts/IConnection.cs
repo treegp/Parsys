6a0ff29cce5f4a76ad5a0c0b16519a0ce32d3d11
Saeed MJ <treegp@gmail.com>
2022-07-21 14:31:23 +0430

    Add Interface

---
+++ Parsys.DataLayer/Connections/ProviderAbstracts/IConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsys.DataLayer.Connections.ProviderAbstracts
+{
+    public interface IConnection
+    {
+            Task<bool> CheckConnection();
+            Task<bool> CheckDatabase();
+            Task<bool> CreateDatabase(string script);
+            void RefreshConnection(string connection);
+    }
+}

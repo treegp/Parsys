8d9f28ea068da7f424596b4c2284d0ba728f0e51
Saeed MJ <treegp@gmail.com>
2022-07-21 19:00:14 +0430

    Add TypeRegistery

---
+++ Parsys.WinClient/IoC/TypesRegistery.cs
@@ -0,0 +1,18 @@
+﻿using Parsys.DataLayer.Connections.ProviderAbstracts;
+using Parsys.DataLayer.Connections.ProviderMethods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsys.IoC
+{
+    public class TypesRegistery :StructureMap.Registry
+    {
+        public TypesRegistery()
+        {
+            For<IConnection>().Use<ConnectToSQL>();
+        }
+    }
+}

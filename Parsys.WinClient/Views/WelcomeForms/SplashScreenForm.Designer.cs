5832a6fdee8f49b93d9593651f0d12ada30ec223
Saeed MJ <treegp@gmail.com>
2022-07-21 19:02:44 +0430

    Design SplashScreen

---
+++ Parsys.WinClient/Views/WelcomeForms/SplashScreenForm.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Parsys.WinClient.Views.WelcomeForms
+{
+    partial class SplashScreenForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplashScreenForm));
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.SplashScreenProgressBar = new System.Windows.Forms.ProgressBar();
+            this.ProgressLabel = new System.Windows.Forms.Label();
+            this.VersionLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.TitleLabel.Font = new System.Drawing.Font("B Yekan", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.TitleLabel.ForeColor = System.Drawing.Color.White;
+            this.TitleLabel.Location = new System.Drawing.Point(232, 160);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(439, 60);
+            this.TitleLabel.TabIndex = 0;
+            this.TitleLabel.Text = "نرم افزار انبارداری پارسیس";
+            // 
+            // SplashScreenProgressBar
+            // 
+            this.SplashScreenProgressBar.BackColor = System.Drawing.Color.Black;
+            this.SplashScreenProgressBar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.SplashScreenProgressBar.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.SplashScreenProgressBar.Location = new System.Drawing.Point(0, 444);
+            this.SplashScreenProgressBar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.SplashScreenProgressBar.Name = "SplashScreenProgressBar";
+            this.SplashScreenProgressBar.RightToLeftLayout = true;
+            this.SplashScreenProgressBar.Size = new System.Drawing.Size(800, 6);
+            this.SplashScreenProgressBar.Step = 500;
+            this.SplashScreenProgressBar.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.SplashScreenProgressBar.TabIndex = 1;
+            this.SplashScreenProgressBar.Value = 20;
+            // 
+            // ProgressLabel
+            // 
+            this.ProgressLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ProgressLabel.BackColor = System.Drawing.Color.Transparent;
+            this.ProgressLabel.Font = new System.Drawing.Font("B Yekan", 7F);
+            this.ProgressLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ProgressLabel.Location = new System.Drawing.Point(56, 422);
+            this.ProgressLabel.Name = "ProgressLabel";
+            this.ProgressLabel.Size = new System.Drawing.Size(729, 17);
+            this.ProgressLabel.TabIndex = 0;
+            this.ProgressLabel.Text = "در حال بارگزاری سیستم";
+            this.ProgressLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // VersionLabel
+            // 
+            this.VersionLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.VersionLabel.AutoSize = true;
+            this.VersionLabel.BackColor = System.Drawing.Color.Transparent;
+            this.VersionLabel.Font = new System.Drawing.Font("B Yekan", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.VersionLabel.ForeColor = System.Drawing.Color.White;
+            this.VersionLabel.Location = new System.Drawing.Point(121, 191);
+            this.VersionLabel.Name = "VersionLabel";
+            this.VersionLabel.Size = new System.Drawing.Size(99, 29);
+            this.VersionLabel.TabIndex = 0;
+            this.VersionLabel.Text = "نسخه 1.0.0";
+            // 
+            // SplashScreenForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Parsys.Properties.Resources.SplashScreen;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SplashScreenProgressBar);
+            this.Controls.Add(this.ProgressLabel);
+            this.Controls.Add(this.VersionLabel);
+            this.Controls.Add(this.TitleLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SplashScreenForm";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Splash Screen";
+            this.Load += new System.EventHandler(this.SplashScreenForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.ProgressBar SplashScreenProgressBar;
+        private System.Windows.Forms.Label ProgressLabel;
+        private System.Windows.Forms.Label VersionLabel;
+    }
+}
+

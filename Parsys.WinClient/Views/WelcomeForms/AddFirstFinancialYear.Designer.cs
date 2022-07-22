d34a4393586a2315a570ad0c5d8c300dc9038c86
Saeed MJ <treegp@gmail.com>
2022-07-22 11:05:23 +0430

    Initialize FinancialYear

---
+++ Parsys.WinClient/Views/WelcomeForms/AddFirstFinancialYear.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Parsys.Views.WelcomeForms
+{
+    partial class AddFirstFinancialYear
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OkButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.StartDateMaskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.CorporationTitleTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TitleTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.DescriptionTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.FinishDateMaskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // OkButton
+            // 
+            this.OkButton.Location = new System.Drawing.Point(12, 8);
+            this.OkButton.Name = "OkButton";
+            this.OkButton.Size = new System.Drawing.Size(91, 25);
+            this.OkButton.TabIndex = 0;
+            this.OkButton.Text = "تایید";
+            this.OkButton.UseVisualStyleBackColor = true;
+            this.OkButton.Click += new System.EventHandler(this.OkButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(308, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "نام شرکت / سازمان";
+            // 
+            // StartDateMaskedTextBox
+            // 
+            this.StartDateMaskedTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.StartDateMaskedTextBox.Location = new System.Drawing.Point(188, 179);
+            this.StartDateMaskedTextBox.Name = "StartDateMaskedTextBox";
+            this.StartDateMaskedTextBox.Size = new System.Drawing.Size(91, 23);
+            this.StartDateMaskedTextBox.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel1.Controls.Add(this.CancelButton);
+            this.panel1.Controls.Add(this.OkButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 246);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(436, 43);
+            this.panel1.TabIndex = 3;
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(109, 8);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(91, 25);
+            this.CancelButton.TabIndex = 0;
+            this.CancelButton.Text = "صرف نظر";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // CorporationTitleTextBox
+            // 
+            this.CorporationTitleTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CorporationTitleTextBox.Enabled = false;
+            this.CorporationTitleTextBox.Location = new System.Drawing.Point(12, 12);
+            this.CorporationTitleTextBox.Name = "CorporationTitleTextBox";
+            this.CorporationTitleTextBox.Size = new System.Drawing.Size(267, 23);
+            this.CorporationTitleTextBox.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(327, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "عنوان سال مالی";
+            // 
+            // TitleTextBox
+            // 
+            this.TitleTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TitleTextBox.Location = new System.Drawing.Point(12, 41);
+            this.TitleTextBox.Name = "TitleTextBox";
+            this.TitleTextBox.Size = new System.Drawing.Size(267, 23);
+            this.TitleTextBox.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(370, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "توضیحات";
+            // 
+            // DescriptionTextBox
+            // 
+            this.DescriptionTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DescriptionTextBox.Location = new System.Drawing.Point(12, 70);
+            this.DescriptionTextBox.Multiline = true;
+            this.DescriptionTextBox.Name = "DescriptionTextBox";
+            this.DescriptionTextBox.Size = new System.Drawing.Size(267, 103);
+            this.DescriptionTextBox.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(298, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "تاریخ افتتاح سال مالی";
+            // 
+            // FinishDateMaskedTextBox
+            // 
+            this.FinishDateMaskedTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.FinishDateMaskedTextBox.Location = new System.Drawing.Point(188, 208);
+            this.FinishDateMaskedTextBox.Name = "FinishDateMaskedTextBox";
+            this.FinishDateMaskedTextBox.Size = new System.Drawing.Size(91, 23);
+            this.FinishDateMaskedTextBox.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(294, 208);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(132, 16);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "تاریخ اختتام سال مالی";
+            // 
+            // AddFirstFinancialYear
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(436, 289);
+            this.Controls.Add(this.DescriptionTextBox);
+            this.Controls.Add(this.TitleTextBox);
+            this.Controls.Add(this.CorporationTitleTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.FinishDateMaskedTextBox);
+            this.Controls.Add(this.StartDateMaskedTextBox);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.MinimumSize = new System.Drawing.Size(454, 336);
+            this.Name = "AddFirstFinancialYear";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "افزودن سال مالی";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OkButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox StartDateMaskedTextBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox CorporationTitleTextBox;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TitleTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox DescriptionTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MaskedTextBox FinishDateMaskedTextBox;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ Parsys.WinClient/Views/WelcomeForms/AddFirstFinancialYear.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parsys.Views.WelcomeForms
+{
+    public partial class AddFirstFinancialYear : Form
+    {
+        public AddFirstFinancialYear()
+        {
+            InitializeComponent();
+        }
+
+        private void OkButton_Click(object sender, EventArgs e)
+        {
+            DialogResult=DialogResult.OK; 
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+        }
+    }
+}

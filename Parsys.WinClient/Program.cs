b781fe7b8cd97458144fae6c14e605bb80ac5304
Saeed MJ <treegp@gmail.com>
2022-07-22 21:43:26 +0430

    Add Procedures to Initialization

--- Parsys.WinClient/Program.cs
+++ Parsys.WinClient/Program.cs
@@ -1,6 +1,11 @@
-﻿using Parsys.WinClient.Views.WelcomeForms;
+﻿using Parsys.DataLayer.Entities.EntityMethods;
+using Parsys.DataLayer.Entities.EntityModels;
+using Parsys.WinClient.Views.WelcomeForms;
 using System;
+using System.Configuration;
+using System.Linq;
 using System.Windows.Forms;
+using Parsys.WinClient.ApplicationMethods;
 
 namespace Parsys.WinClient
 {
@@ -16,10 +21,60 @@ namespace Parsys.WinClient
             Application.SetCompatibleTextRenderingDefault(false);
 
             var container = new StructureMap.Container(new IoC.TypesRegistery());
-            var splashScreen = container.GetInstance<SplashScreenForm>();
-            var result = splashScreen.ShowDialog();
-            if (result != DialogResult.OK)
+            var splashScreenForm = container.GetInstance<SplashScreenForm>();
+            var splashScreenResult = splashScreenForm.ShowDialog();
+            if (splashScreenResult != DialogResult.OK)
+            {
+                Application.Exit();
                 return;
+            }
+
+            var connection = ConfigurationManager.ConnectionStrings["PARSYS"].ConnectionString;
+            CorporationsRepository corpo = new CorporationsRepository(connection);
+
+            if (corpo.Count() == 0)
+            {
+                Corporations firstCorporation = new Corporations();
+
+                AddFirstCorporation corpoForm = new AddFirstCorporation(firstCorporation);
+                var corpoResult = corpoForm.ShowDialog();
+
+                if (corpoResult != DialogResult.OK)
+                {
+                    Application.Exit();
+                    return;
+                }
+                corpo.Insert(firstCorporation);
+            }
+
+
+            FinancialYearsRepository finance = new FinancialYearsRepository(connection);
+
+            var corpoId = corpo.GetAll().Select(c => c.Id).First();
+            if (!finance.GetByCorporationId(corpoId).Any())
+            {
+                FinancialYears firstFinancialYear = new FinancialYears();
+                
+                AddFirstFinancialYear financeForm = new AddFirstFinancialYear(corpo.GetById(corpoId).Select(c=>c.Title).First());
+                var financeResult = financeForm.ShowDialog();
+
+                if (financeResult != DialogResult.OK)
+                {
+                    Application.Exit();
+                    return;
+                }
+
+                firstFinancialYear.CorporationId = corpoId;
+                firstFinancialYear.Title = financeForm.FinancialYear.Title;
+                firstFinancialYear.Description= financeForm.FinancialYear.Description;
+                firstFinancialYear.StartDate = (DateTime) DateTimeConverter.ShamsiStringToMiladiDateTime(financeForm.FinancialYear.StartDate);
+                firstFinancialYear.FinishDate = (DateTime)DateTimeConverter.ShamsiStringToMiladiDateTime(financeForm.FinancialYear.FinishDate);
+
+                finance.Insert(firstFinancialYear);
+
+            }
+
+
 
             Application.Run(new MainForm());
         }

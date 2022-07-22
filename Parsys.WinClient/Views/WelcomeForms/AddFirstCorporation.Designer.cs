cb8f786a66e8e3e26722cf2f5617f88a6c275084
Saeed MJ <treegp@gmail.com>
2022-07-22 11:05:03 +0430

    Initialize Corporation

---
+++ Parsys.WinClient/Views/WelcomeForms/AddFirstCorporation.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Parsys.Views.WelcomeForms
+{
+    partial class AddFirstCorporation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OkButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TitleTextBox = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TelephoneTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.FaxTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.AddressTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.DescriptionTextBox = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // OkButton
+            // 
+            this.OkButton.Location = new System.Drawing.Point(13, 9);
+            this.OkButton.Name = "OkButton";
+            this.OkButton.Size = new System.Drawing.Size(91, 25);
+            this.OkButton.TabIndex = 0;
+            this.OkButton.Text = "تایید";
+            this.OkButton.UseVisualStyleBackColor = true;
+            this.OkButton.Click += new System.EventHandler(this.OkButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(368, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "عنوان";
+            // 
+            // TitleTextBox
+            // 
+            this.TitleTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TitleTextBox.Location = new System.Drawing.Point(12, 14);
+            this.TitleTextBox.Name = "TitleTextBox";
+            this.TitleTextBox.Size = new System.Drawing.Size(330, 23);
+            this.TitleTextBox.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel1.Controls.Add(this.CancelButton);
+            this.panel1.Controls.Add(this.OkButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 281);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(415, 43);
+            this.panel1.TabIndex = 5;
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(110, 9);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(91, 25);
+            this.CancelButton.TabIndex = 1;
+            this.CancelButton.Text = "صرف نظر";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(373, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "تلفن";
+            // 
+            // TelephoneTextBox
+            // 
+            this.TelephoneTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TelephoneTextBox.Location = new System.Drawing.Point(12, 43);
+            this.TelephoneTextBox.Name = "TelephoneTextBox";
+            this.TelephoneTextBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.TelephoneTextBox.Size = new System.Drawing.Size(330, 23);
+            this.TelephoneTextBox.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(370, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "فکس";
+            // 
+            // FaxTextBox
+            // 
+            this.FaxTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FaxTextBox.Location = new System.Drawing.Point(12, 72);
+            this.FaxTextBox.Name = "FaxTextBox";
+            this.FaxTextBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.FaxTextBox.Size = new System.Drawing.Size(330, 23);
+            this.FaxTextBox.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(370, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "آدرس";
+            // 
+            // AddressTextBox
+            // 
+            this.AddressTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.AddressTextBox.Location = new System.Drawing.Point(12, 102);
+            this.AddressTextBox.Multiline = true;
+            this.AddressTextBox.Name = "AddressTextBox";
+            this.AddressTextBox.Size = new System.Drawing.Size(330, 70);
+            this.AddressTextBox.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(351, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 16);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "توضیحات";
+            // 
+            // DescriptionTextBox
+            // 
+            this.DescriptionTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DescriptionTextBox.Location = new System.Drawing.Point(12, 181);
+            this.DescriptionTextBox.Multiline = true;
+            this.DescriptionTextBox.Name = "DescriptionTextBox";
+            this.DescriptionTextBox.Size = new System.Drawing.Size(330, 77);
+            this.DescriptionTextBox.TabIndex = 4;
+            // 
+            // AddFirstCorporation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(415, 324);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.DescriptionTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.FaxTextBox);
+            this.Controls.Add(this.AddressTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TelephoneTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TitleTextBox);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.MinimumSize = new System.Drawing.Size(433, 371);
+            this.Name = "AddFirstCorporation";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "افزودن شرکت/سازمان";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OkButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TitleTextBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TelephoneTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox FaxTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox AddressTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox DescriptionTextBox;
+    }
+}
---
+++ Parsys.WinClient/Views/WelcomeForms/AddFirstCorporation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parsys.Views.WelcomeForms
+{
+    public partial class AddFirstCorporation : Form
+    {
+        public AddFirstCorporation()
+        {
+            InitializeComponent();
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+        }
+
+        private void OkButton_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+        }
+    }
+}

0e13cab99a260499d7aa6405144d8ae23bc08509
Saeed MJ <treegp@gmail.com>
2022-07-24 16:20:09 +0430

    Add MainBaseForm

---
+++ Parsys.WinClient/Views/Bases/MainBaseForm.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Parsys.WinClient.Views.Bases
+{
+    partial class MainBaseForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.MainStatusStrip = new System.Windows.Forms.StatusStrip();
+            this.MainTabPage = new System.Windows.Forms.TabPage();
+            this.MainTabControl = new System.Windows.Forms.TabControl();
+            this.عملیاتToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.خروجToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MainMenuStrip.SuspendLayout();
+            this.MainTabControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainMenuStrip
+            // 
+            this.MainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.عملیاتToolStripMenuItem});
+            this.MainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.MainMenuStrip.Name = "MainMenuStrip";
+            this.MainMenuStrip.Padding = new System.Windows.Forms.Padding(7, 2, 0, 2);
+            this.MainMenuStrip.Size = new System.Drawing.Size(863, 24);
+            this.MainMenuStrip.TabIndex = 0;
+            this.MainMenuStrip.Text = "menuStrip1";
+            // 
+            // MainStatusStrip
+            // 
+            this.MainStatusStrip.Location = new System.Drawing.Point(0, 576);
+            this.MainStatusStrip.Name = "MainStatusStrip";
+            this.MainStatusStrip.Size = new System.Drawing.Size(863, 22);
+            this.MainStatusStrip.TabIndex = 1;
+            this.MainStatusStrip.Text = "statusStrip1";
+            // 
+            // MainTabPage
+            // 
+            this.MainTabPage.Location = new System.Drawing.Point(4, 23);
+            this.MainTabPage.Name = "MainTabPage";
+            this.MainTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.MainTabPage.Size = new System.Drawing.Size(855, 525);
+            this.MainTabPage.TabIndex = 0;
+            this.MainTabPage.Text = "صفحه اصلی";
+            this.MainTabPage.UseVisualStyleBackColor = true;
+            // 
+            // MainTabControl
+            // 
+            this.MainTabControl.Controls.Add(this.MainTabPage);
+            this.MainTabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainTabControl.Location = new System.Drawing.Point(0, 24);
+            this.MainTabControl.Name = "MainTabControl";
+            this.MainTabControl.RightToLeftLayout = true;
+            this.MainTabControl.SelectedIndex = 0;
+            this.MainTabControl.Size = new System.Drawing.Size(863, 552);
+            this.MainTabControl.TabIndex = 2;
+            this.MainTabControl.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.MainTabControl_DrawItem);
+            this.MainTabControl.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainTabControl_MouseDown);
+            // 
+            // عملیاتToolStripMenuItem
+            // 
+            this.عملیاتToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.خروجToolStripMenuItem});
+            this.عملیاتToolStripMenuItem.Name = "عملیاتToolStripMenuItem";
+            this.عملیاتToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.عملیاتToolStripMenuItem.Text = "عملیات";
+            // 
+            // خروجToolStripMenuItem
+            // 
+            this.خروجToolStripMenuItem.Name = "خروجToolStripMenuItem";
+            this.خروجToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.خروجToolStripMenuItem.Text = "خروج";
+            // 
+            // MainBaseForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(863, 598);
+            this.Controls.Add(this.MainTabControl);
+            this.Controls.Add(this.MainStatusStrip);
+            this.Controls.Add(this.MainMenuStrip);
+            this.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.MainMenuStrip = this.MainMenuStrip;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "MainBaseForm";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "پارسیس";
+            this.MainMenuStrip.ResumeLayout(false);
+            this.MainMenuStrip.PerformLayout();
+            this.MainTabControl.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip MainMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem عملیاتToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem خروجToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip MainStatusStrip;
+        private System.Windows.Forms.TabPage MainTabPage;
+        private System.Windows.Forms.TabControl MainTabControl;
+    }
+}

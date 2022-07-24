bbbc8e10f9809079c1880416a152d87314d81344
Saeed MJ <treegp@gmail.com>
2022-07-24 16:19:59 +0430

    Add MainBaseForm

---
+++ Parsys.WinClient/Views/Bases/MainBaseForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parsys.WinClient.Views.Bases
+{
+    public partial class MainBaseForm : Form
+    {
+        public MainBaseForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainTabControl_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            //This code will render a "x" mark at the end of the Tab caption. 
+            e.Graphics.DrawString("x", e.Font, Brushes.Black, e.Bounds.Right - 15, e.Bounds.Top + 4);
+            e.Graphics.DrawString(this.MainTabControl.TabPages[e.Index].Text, e.Font, Brushes.Black, e.Bounds.Left + 12, e.Bounds.Top + 4);
+            e.DrawFocusRectangle();
+        }
+
+        private void MainTabControl_MouseDown(object sender, MouseEventArgs e)
+        {
+            //Looping through the controls.
+            for (int i = 0; i < this.MainTabControl.TabPages.Count; i++)
+            {
+                Rectangle r = MainTabControl.GetTabRect(i);
+                //Getting the position of the "x" mark.
+                Rectangle closeButton = new Rectangle(r.Left - 15, r.Top + 4, 9, 7);
+                if (closeButton.Contains(e.Location))
+                {
+                    if (MessageBox.Show("Would you like to Close this Tab?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
+                        this.MainTabControl.TabPages.RemoveAt(i);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}

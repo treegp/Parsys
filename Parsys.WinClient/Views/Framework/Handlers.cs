66c6b0eb62d8935fa956f7316ee2ec74c63d309e
Saeed MJ <treegp@gmail.com>
2022-07-25 16:19:09 +0430

    Add MenuStripHandler into Handlers Class

---
+++ Parsys.WinClient/Views/Framework/Handlers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Parsys.WinClient.Views.Framework
+{
+
+    public class MenuStripHandler
+    {
+        private ToolStripItemCollection itemsHierarchy;
+
+        public MenuStripHandler(ToolStripItemCollection collection)
+        {
+            itemsHierarchy = collection;
+        }
+
+        public MenuStripHandler AddMenuItem(string caption)
+        {
+            var menu = (ToolStripMenuItem)itemsHierarchy.Add(caption, null, null);
+            return new MenuStripHandler(menu.DropDownItems);
+        }
+
+        public MenuStripHandler AddMenuItem(string caption, EventHandler e)
+        {
+            var menu = (ToolStripMenuItem)itemsHierarchy.Add(caption, null, e);
+            return new MenuStripHandler(menu.DropDownItems);
+        }
+
+
+        public MenuStripHandler AddMenuItem(string caption, Image thumbnail, EventHandler e)
+        {
+            var menu = (ToolStripMenuItem)itemsHierarchy.Add(caption, thumbnail, e);
+            return new MenuStripHandler(menu.DropDownItems);
+        }
+    }
+}

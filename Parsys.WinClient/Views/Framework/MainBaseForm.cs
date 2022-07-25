14860f0adfef5b67599b7060bc377602db1c58b0
Saeed MJ <treegp@gmail.com>
2022-07-25 16:19:21 +0430

    Update BaseForm

--- Parsys.WinClient/Views/Framework/MainBaseForm.cs
+++ Parsys.WinClient/Views/Framework/MainBaseForm.cs
@@ -35,34 +35,6 @@ namespace Parsys.WinClient.Views.Framework
 
 
 
-    public class MenuStripHandler
-    {
-        private ToolStripItemCollection itemsHierarchy;
-
-        public MenuStripHandler(ToolStripItemCollection collection)
-        {
-            itemsHierarchy = collection;
-        }
-
-        public MenuStripHandler AddMenuItem(string caption)
-        {
-            var menu = (ToolStripMenuItem)itemsHierarchy.Add(caption, null, null);
-            return new MenuStripHandler(menu.DropDownItems);
-        }
-
-        public MenuStripHandler AddMenuItem(string caption, EventHandler e)
-        {
-            var menu = (ToolStripMenuItem)itemsHierarchy.Add(caption, null, e);
-            return new MenuStripHandler(menu.DropDownItems);
-        }
-
-
-        public MenuStripHandler AddMenuItem(string caption, Image thumbnail, EventHandler e)
-        {
-            var menu = (ToolStripMenuItem)itemsHierarchy.Add(caption, thumbnail, e);
-            return new MenuStripHandler(menu.DropDownItems);
-        }
-    }
 
 }
 

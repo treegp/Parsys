e6e680fe2b5239d90745cf1c021be02d48364628
Saeed MJ <treegp@gmail.com>
2022-07-29 11:36:48 +0430

    Add TabBaseControl

---
+++ Parsys.WinClient/Views/Framework/TabBaseControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Forms;
+
+namespace Parsys.WinClient.Views.Framework
+{
+    public partial class TabBaseControl : UserControl
+    {
+        public string Title { get; set; }
+        public string Id { get; set; }
+        public bool MultipleInstance { get; set; } = false;
+
+        public TabBaseControl()
+        {
+            InitializeComponent();
+        }
+    }
+}

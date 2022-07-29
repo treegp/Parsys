dd61cd76efc18503117669a72677a5f647ccbcf4
Saeed MJ <treegp@gmail.com>
2022-07-29 10:37:19 +0430

    Change FinacialYearFormModel

--- Parsys.WinClient/Views/WelcomeForms/AddFirstFinancialYear.cs
+++ Parsys.WinClient/Views/WelcomeForms/AddFirstFinancialYear.cs
@@ -1,6 +1,6 @@
 ﻿using System;
+using System.Globalization;
 using System.Windows.Forms;
-using Parsys.DataLayer.Entities.EntityModels;
 
 
 namespace Parsys.WinClient.Views.WelcomeForms
@@ -10,12 +10,6 @@ namespace Parsys.WinClient.Views.WelcomeForms
 
         public FinancialYearFormModel FinancialYear = new FinancialYearFormModel();
 
-        private string startDateBinder;
-        private string finishDateBinder;
-       
-        
-
-
         public AddFirstFinancialYear(string corporationTitle)
         {
             InitializeComponent();
@@ -23,16 +17,22 @@ namespace Parsys.WinClient.Views.WelcomeForms
             CorporationTitleTextBox.Text = corporationTitle;
             TitleTextBox.DataBindings.Add("Text", FinancialYear, "Title");
             DescriptionTextBox.DataBindings.Add("Text", FinancialYear, "Description");
-            StartDateMaskedTextBox.DataBindings.Add("Text", FinancialYear, "StartDate");
-            FinishDateMaskedTextBox.DataBindings.Add("Text", FinancialYear, "FinishDate");
+            StartDateMaskedTextBox.DataBindings.Add("Text", FinancialYear, "StartDateString");
+            FinishDateMaskedTextBox.DataBindings.Add("Text", FinancialYear, "FinishDateString");
 
-
-            
         }
 
         private void OkButton_Click(object sender, EventArgs e)
         {
-            DialogResult=DialogResult.OK; 
+            FormErrorProvider.Clear();
+            if (TitleTextBox.Text == "")
+                FormErrorProvider.SetError(TitleTextBox, "عنوان نمی تواند خالی باشد");
+            else if (FinancialYear.StartDate.Year == 1)
+                FormErrorProvider.SetError(StartDateMaskedTextBox, "تاریخ افتتاح سال مالی نامعتبر است");
+            else if (FinancialYear.FinishDate.Year == 1)
+                FormErrorProvider.SetError(FinishDateMaskedTextBox, "تاریخ اختتام سال مالی نامعتبر است");
+            else
+                DialogResult = DialogResult.OK;
         }
 
         private void CancelButton_Click(object sender, EventArgs e)
@@ -43,10 +43,50 @@ namespace Parsys.WinClient.Views.WelcomeForms
 
         public class FinancialYearFormModel
         {
+
+
             public string Title { get; set; }
             public string Description { get; set; }
-            public string StartDate { get; set; }
-            public string  FinishDate { get; set; }
+            public string StartDateString { get; set; }
+            public string FinishDateString { get; set; }
+
+            public DateTime StartDate
+            {
+                get
+                {
+                    try
+                    {
+                        DateTime dt = new DateTime(int.Parse(StartDateString.Split('/')[0]),
+                            int.Parse(StartDateString.Split('/')[1]), int.Parse(StartDateString.Split('/')[2]),
+                            new PersianCalendar());
+                        return dt;
+                    }
+                    catch
+                    {
+                        return new DateTime(1, 1, 1);
+                    }
+
+                }
+            }
+
+            public DateTime FinishDate
+            {
+                get
+                {
+                    try
+                    {
+                        DateTime dt = new DateTime(int.Parse(FinishDateString.Split('/')[0]),
+                            int.Parse(FinishDateString.Split('/')[1]), int.Parse(FinishDateString.Split('/')[2]),
+                            new PersianCalendar());
+                        return dt;
+                    }
+                    catch
+                    {
+                        return new DateTime(1, 1, 1);
+                    }
+                }
+            }
+
         }
     }
 }

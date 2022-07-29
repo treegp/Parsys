9179740ac6390ab6fe77cdfdd0a0a8f26ac75799
Saeed MJ <treegp@gmail.com>
2022-07-29 15:24:04 +0430

    Build for Show Views in Form or Tabs

---
+++ Parsys.WinClient/Views/Framework/ViewBaseControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Forms;
+
+namespace Parsys.WinClient.Views.Framework
+{
+    public partial class ViewBaseControl : UserControl
+    {
+        public string Title { get; set; }
+        public string Id { get; set; }
+        public bool MultipleInstance { get; set; } = false;
+
+        public ViewBaseControl()
+        {
+            InitializeComponent();
+        }
+    }
+}

a895a37653c16c990df09540feacab07a244dcb2
Saeed MJ <treegp@gmail.com>
2022-07-30 20:32:19 +0430

    Editor Form Control

---
+++ Parsys.WinClient/Views/CorporationForms/Editor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Parsys.WinClient.Views.Framework;
+
+namespace Parsys.WinClient.Views.CorporationForms
+{
+    public partial class Editor : ViewBaseControl
+    {
+        public Editor()
+        {
+            InitializeComponent();
+            
+            Id = "EditorCorporation";
+            Title = "ویرایش شرکت/سازمان";
+            MultipleInstance=false;
+        }
+
+        public string caption;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ViewManagement.OpenTab<Add>();
+        }
+    }
+}

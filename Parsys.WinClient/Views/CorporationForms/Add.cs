8732898b99f89c2bbb3dd97ee09fa629d1bb7cfe
Saeed MJ <treegp@gmail.com>
2022-07-30 20:31:55 +0430

    Add UserControl

---
+++ Parsys.WinClient/Views/CorporationForms/Add.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Parsys.WinClient.Views.Framework;
+
+namespace Parsys.WinClient.Views.CorporationForms
+{
+    public partial class Add : ViewBaseControl
+    {
+        public Add()
+        {
+            InitializeComponent();
+            Id = "AddCorporation";
+            Title = "افزودن شرکت / سازمان";
+            MultipleInstance=true;
+        }
+    }
+}

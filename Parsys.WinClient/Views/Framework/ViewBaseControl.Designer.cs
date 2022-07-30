147881a29851db7e5bb4dc23094bfbbb41230863
Saeed MJ <treegp@gmail.com>
2022-07-30 20:34:09 +0430

    Add Files

--- Parsys.WinClient/Views/Framework/ViewBaseControl.Designer.cs
+++ Parsys.WinClient/Views/Framework/ViewBaseControl.Designer.cs
@@ -28,10 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.ButtonsPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // ButtonsPanel
+            // 
+            this.ButtonsPanel.BackColor = System.Drawing.Color.Gainsboro;
+            this.ButtonsPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ButtonsPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.ButtonsPanel.Location = new System.Drawing.Point(0, 444);
+            this.ButtonsPanel.Name = "ButtonsPanel";
+            this.ButtonsPanel.Size = new System.Drawing.Size(719, 39);
+            this.ButtonsPanel.TabIndex = 0;
+            // 
+            // ViewBaseControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.ButtonsPanel);
+            this.Name = "ViewBaseControl";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Size = new System.Drawing.Size(719, 483);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel ButtonsPanel;
     }
 }

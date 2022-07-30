49833d77967d9017c4fdd1c6ce1042b9acb15f7b
Saeed MJ <treegp@gmail.com>
2022-07-30 20:33:48 +0430

    Updated

--- Parsys.WinClient/Views/Framework/ViewBaseControl.cs
+++ Parsys.WinClient/Views/Framework/ViewBaseControl.cs
@@ -4,6 +4,8 @@ namespace Parsys.WinClient.Views.Framework
 {
     public partial class ViewBaseControl : UserControl
     {
+        public ViewHandler ViewManagement { get; internal set; }
+
         public string Title { get; set; }
         public string Id { get; set; }
         public bool MultipleInstance { get; set; } = false;

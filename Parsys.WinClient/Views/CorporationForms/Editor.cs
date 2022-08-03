cb6ff0086969f0675090cd9aa76a9cdaadaebe18
Saeed MJ <treegp@gmail.com>
2022-08-03 13:18:07 +0430

    Set Add View to Form

--- Parsys.WinClient/Views/CorporationForms/Editor.cs
+++ Parsys.WinClient/Views/CorporationForms/Editor.cs
@@ -25,7 +25,7 @@ namespace Parsys.WinClient.Views.CorporationForms
 
         private void button1_Click(object sender, EventArgs e)
         {
-            ViewManagement.OpenTab<Add>(i =>
+            ViewManagement.ShowForm<Add>(i =>
             {
                 i.Id += textBox1.Text;
                 i.Title += textBox1.Text;

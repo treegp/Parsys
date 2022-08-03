939cf5cf06d57eb200200dbb70b7f6307af7d628
Saeed MJ <treegp@gmail.com>
2022-08-03 09:39:19 +0430

    Redesign BaseControl

--- Parsys.WinClient/Views/Framework/ViewBaseControl.Designer.cs
+++ Parsys.WinClient/Views/Framework/ViewBaseControl.Designer.cs
@@ -36,19 +36,22 @@
             this.ButtonsPanel.BackColor = System.Drawing.Color.Gainsboro;
             this.ButtonsPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.ButtonsPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.ButtonsPanel.Location = new System.Drawing.Point(0, 444);
+            this.ButtonsPanel.Location = new System.Drawing.Point(0, 353);
+            this.ButtonsPanel.Margin = new System.Windows.Forms.Padding(2);
             this.ButtonsPanel.Name = "ButtonsPanel";
-            this.ButtonsPanel.Size = new System.Drawing.Size(719, 39);
+            this.ButtonsPanel.Size = new System.Drawing.Size(539, 39);
             this.ButtonsPanel.TabIndex = 0;
+            this.ButtonsPanel.Visible = false;
             // 
             // ViewBaseControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.ButtonsPanel);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "ViewBaseControl";
             this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.Size = new System.Drawing.Size(719, 483);
+            this.Size = new System.Drawing.Size(539, 392);
             this.ResumeLayout(false);
 
         }

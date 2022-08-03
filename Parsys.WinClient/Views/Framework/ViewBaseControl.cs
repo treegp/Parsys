445f1ca41020731f71541c08de77bea6824f083f
Saeed MJ <treegp@gmail.com>
2022-08-03 09:38:43 +0430

    Fix Close View

--- Parsys.WinClient/Views/Framework/ViewBaseControl.cs
+++ Parsys.WinClient/Views/Framework/ViewBaseControl.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 
 namespace Parsys.WinClient.Views.Framework
 {
@@ -10,9 +11,27 @@ namespace Parsys.WinClient.Views.Framework
         public bool MultipleInstance { get; set; } = false;
         public ViewHandler ViewManagement { get; internal set; }
 
+
+
+        protected Button AddButtun(string title,Action<Button> onPress)
+        {
+            ButtonsPanel.Visible=true;
+            int bottunsCount = ButtonsPanel.Controls.Count;
+            int left = 20 + (90 + 5) * bottunsCount;
+            var btn = new Button();
+            btn.Text = title;
+            btn.Click += (o,e) => onPress(btn);
+            btn.Size = new System.Drawing.Size(90, 25);
+            btn.Location = new System.Drawing.Point(left, 6);
+            ButtonsPanel.Controls.Add(btn);
+            return btn;
+        }
+
         public ViewBaseControl()
         {
             InitializeComponent();
         }
+
+      
     }
 }

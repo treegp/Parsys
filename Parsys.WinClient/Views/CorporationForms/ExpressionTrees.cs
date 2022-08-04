0b387bfe16b2d20ec492a10b13cf5c271ac4ad6b
Saeed MJ <treegp@gmail.com>
2022-08-04 20:12:04 +0430

    Extract a lambda expression

--- Parsys.WinClient/Views/CorporationForms/ExpressionTrees.cs
+++ Parsys.WinClient/Views/CorporationForms/ExpressionTrees.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Parsys.WinClient.Views.CorporationForms
@@ -19,9 +12,19 @@ namespace Parsys.WinClient.Views.CorporationForms
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Func<int,int,int> add = (num1,num2) => num1 + num2;
-            
-            MessageBox.Show(add(2,6).ToString());
+            var n1 = System.Linq.Expressions.Expression.Parameter(typeof(int), "num1");
+            var n2 = System.Linq.Expressions.Expression.Parameter(typeof(int), "num2");
+
+            var addExpression = System.Linq.Expressions.Expression.Add(n1, n2);
+
+            var lambdaExpression = System.Linq.Expressions.Expression.Lambda<Func<int, int, int>>(addExpression, n1, n2);
+
+
+
+
+            Func<int, int, int> add = lambdaExpression.Compile();
+
+            MessageBox.Show(add(2, 6).ToString());
 
         }
     }

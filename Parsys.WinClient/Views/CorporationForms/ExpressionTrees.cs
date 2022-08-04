956d8d1291ce1c732f2215c0884a46b2b4421d6e
Saeed MJ <treegp@gmail.com>
2022-08-04 23:58:59 +0430

    Add Visitor Class

--- Parsys.WinClient/Views/CorporationForms/ExpressionTrees.cs
+++ Parsys.WinClient/Views/CorporationForms/ExpressionTrees.cs
@@ -12,18 +12,13 @@ namespace Parsys.WinClient.Views.CorporationForms
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var n = System.Linq.Expressions.Expression.Parameter(typeof(int), "num");
-            var cons = System.Linq.Expressions.Expression.Constant(2);
-
-            var exp = System.Linq.Expressions.Expression.Multiply(n, cons);
-
-
-            var toString = System.Linq.Expressions.Expression.Call(exp, typeof(object).GetMethod("ToString"));
-
-            var lambda = System.Linq.Expressions.Expression.Lambda<Func<int, string>>(toString, n);
-
-            MessageBox.Show(lambda.Compile()(5));
+            Func<int, int> function = n => n * 2;
 
         }
+    }
+
+    public class MyExpressionVisitor : System.Linq.Expressions.ExpressionVisitor
+    {
+        override  
     }
 }

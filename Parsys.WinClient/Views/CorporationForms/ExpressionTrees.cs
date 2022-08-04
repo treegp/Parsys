ef177f978c559734b69aaa98ade8a9049446293f
Saeed MJ <treegp@gmail.com>
2022-08-04 23:47:15 +0430

    change lambda expression

--- Parsys.WinClient/Views/CorporationForms/ExpressionTrees.cs
+++ Parsys.WinClient/Views/CorporationForms/ExpressionTrees.cs
@@ -12,21 +12,14 @@ namespace Parsys.WinClient.Views.CorporationForms
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var n1 = System.Linq.Expressions.Expression.Parameter(typeof(int), "num1");
-            var n2 = System.Linq.Expressions.Expression.Parameter(typeof(int), "num2");
+            var n = System.Linq.Expressions.Expression.Parameter(typeof(int), "num");
+            var cons = System.Linq.Expressions.Expression.Constant(2);
 
-            var addExpression = System.Linq.Expressions.Expression.Add(n1, n2);
-            var divExpression = System.Linq.Expressions.Expression.Divide(n1, n2);
+            var exp = System.Linq.Expressions.Expression.Multiply(n, cons);
 
-            var finalExpression = System.Linq.Expressions.Expression.Multiply(addExpression, divExpression);
-            var lambdaExpression = System.Linq.Expressions.Expression.Lambda<Func<int, int, int>>(finalExpression, n1, n2);
+            var lambda = System.Linq.Expressions.Expression.Lambda<Func<int, int>>(exp,n);
 
-
-
-
-            Func<int, int, int> add = lambdaExpression.Compile();
-
-            MessageBox.Show(add(12, 6).ToString());
+            MessageBox.Show(lambda.Compile()(5).ToString());
 
         }
     }

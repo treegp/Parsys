fc8dea8a7acf6eb91a5fa1cde1450ebf57db6059
Saeed MJ <treegp@gmail.com>
2022-08-06 01:21:29 +0430

    Add Grid to Inventory List

--- Parsys.WinClient/Views/InventoryForms/List.cs
+++ Parsys.WinClient/Views/InventoryForms/List.cs
@@ -7,15 +7,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Parsys.DataLayer.Entities.EntityAbstracts;
 using Parsys.WinClient.Views.Framework;
 
 namespace Parsys.WinClient.Views.InventoryForms
 {
+    
+
     public partial class List : ViewBaseControl
     {
-        public List()
+        private IInventoriesRepository invRepo;
+        public List(IInventoriesRepository repo)
         {
             InitializeComponent();
+            invRepo = repo;
+        }
+
+
+        protected override void OnLoad(EventArgs e)
+        {
+            var grid = new GridHandler<DataLayer.Entities.EntityModels.Inventories>(this, invRepo.GetAll());
+
+            grid.AddStringColumn(i => i.Id,"شناسع");
+            grid.AddStringColumn(i => i.CorporationId,"شناسه شرکت");
+            grid.AddStringColumn(i => i.Title,"عنوان");
+            grid.AddStringColumn(i => i.Description);
+            grid.AddStringColumn(i => i.IsDeleted);
+            
+
+
+            base.OnLoad(e);
         }
     }
 }

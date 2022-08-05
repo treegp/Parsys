c0773b9344a090270c31f261bf497a57091b49cf
Saeed MJ <treegp@gmail.com>
2022-08-05 18:52:33 +0430

    Add built-in Grid to show liost of Corporation

--- Parsys.WinClient/Views/CorporationForms/List.cs
+++ Parsys.WinClient/Views/CorporationForms/List.cs
@@ -1,5 +1,8 @@
-﻿using System.Windows.Forms;
+﻿using Parsys.DataLayer.Entities.EntityModels;
 using Parsys.WinClient.Views.Framework;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
 
 namespace Parsys.WinClient.Views.CorporationForms
 {
@@ -14,12 +17,61 @@ namespace Parsys.WinClient.Views.CorporationForms
 
             AddButtun("ویرایش مشتری", b => { ViewManagement.OpenTab<Editor>(); });
 
-
         }
 
-        private void button1_Click(object sender, System.EventArgs e)
+        protected override void OnLoad(EventArgs e)
         {
-            ViewManagement.OpenTab<Editor>(c=>c.caption="آی دی 12");
+            List<Products> products = new List<Products>();
+            products.Add(new Products() { Id = 1, Title = "محصول یک", Code = "One", Description = "ندارد" });
+            products.Add(new Products() { Id = 1, Title = "محصول دو", Code = "Two", Description = "ندارد" });
+            products.Add(new Products() { Id = 1, Title = "محصول سه", Code = "Three", Description = "ندارد" });
+            products.Add(new Products() { Id = 1, Title = "محصول چهار", Code = "Four", Description = "ندارد" });
+            products.Add(new Products() { Id = 1, Title = "محصول پنج", Code = "Five", Description = "ندارد" });
+
+
+
+            var grid = new DataGridView();
+            grid.Dock = DockStyle.Fill;
+            grid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            grid.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            grid.AllowUserToAddRows = false;
+            grid.AllowUserToDeleteRows = false;
+            grid.EditMode = DataGridViewEditMode.EditProgrammatically;
+            grid.AutoGenerateColumns=false;
+            grid.MultiSelect=false;
+            grid.DataSource=products;
+
+            
+
+            grid.Columns.Add(new DataGridViewTextBoxColumn()
+            {
+                HeaderText = "آی دی",
+                DataPropertyName = "Id"
+            });
+
+            grid.Columns.Add(new DataGridViewTextBoxColumn()
+            {
+                HeaderText = "عنوان",
+                DataPropertyName = "Title"
+            });
+
+            grid.Columns.Add(new DataGridViewTextBoxColumn()
+            {
+                HeaderText = "کد محصول",
+                DataPropertyName = "Code"
+            });
+
+            grid.Columns.Add(new DataGridViewTextBoxColumn()
+            {
+                HeaderText = "توضیحات",
+                DataPropertyName = "Description"
+            });
+
+
+            this.Controls.Add(grid);
+
+
         }
+
     }
 }

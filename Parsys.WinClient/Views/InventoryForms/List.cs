119d2cbce67635506e2a3a6406250a1e33fb8273
Saeed MJ <treegp@gmail.com>
2022-08-06 01:11:50 +0430

    Add Inventory List to Project

---
+++ Parsys.WinClient/Views/InventoryForms/List.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Parsys.WinClient.Views.Framework;
+
+namespace Parsys.WinClient.Views.InventoryForms
+{
+    public partial class List : ViewBaseControl
+    {
+        public List()
+        {
+            InitializeComponent();
+        }
+    }
+}

8a87fde4ce5be98ac9bd689c6f8c3f890d44855e
Saeed MJ <treegp@gmail.com>
2022-08-08 10:58:38 +0430

    Add buttons to handle view

--- Parsys.WinClient/Views/Framework/EntityEditorClass.cs
+++ Parsys.WinClient/Views/Framework/EntityEditorClass.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Parsys.WinClient.Views.Framework
 {
-    internal class EntityEditorClass
+    public class EntityEditorClass<TEntity> : ViewBaseControl where TEntity : class
     {
+        public TEntity Entity { get; set; }
+
+
+        public EntityEditorClass()
+        {
+
+
+            AddButtun("تائید", b => CloseThis(DialogResult.OK));
+            AddButtun("صرف نظر", b => CloseThis(DialogResult.Cancel));
+        }
+
+
+
     }
 }

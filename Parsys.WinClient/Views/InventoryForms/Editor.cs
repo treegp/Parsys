c16cebaffe4ee45be37bf269836b7d7c87554923
Saeed MJ <treegp@gmail.com>
2022-08-08 11:19:59 +0430

    Add properties to load view

---
+++ Parsys.WinClient/Views/InventoryForms/Editor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Parsys.DataLayer.Entities.EntityModels;
+using Parsys.WinClient.Views.Framework;
+
+namespace Parsys.WinClient.Views.InventoryForms
+{
+    public class Editor : EntityEditorClass<Inventories>
+    {
+
+        public Editor()
+        {
+
+            Id = "EditInventories";
+            Title = "";
+            MultipleInstance = false;
+        }
+
+    }
+}

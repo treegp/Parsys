8a4337a46e428830b86bd863c1735b3c03f123c4
Saeed MJ <treegp@gmail.com>
2022-08-08 12:09:47 +0430

    Add TextBox Control to View

--- Parsys.WinClient/Views/Framework/EntityEditorClass.cs
+++ Parsys.WinClient/Views/Framework/EntityEditorClass.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -20,6 +21,35 @@ namespace Parsys.WinClient.Views.Framework
             AddButtun("صرف نظر", b => CloseThis(DialogResult.Cancel));
         }
 
+
+
+        protected TextBox newTextBox<TReturn>(Expression<Func<TEntity,TReturn>> item,string caption)
+        {
+            Label label = new Label();
+            label.Text = caption;
+
+            TextBox textBox = new TextBox();
+
+
+
+
+            this.Controls.Add(label);
+            this.Controls.Add(textBox);
+            return textBox;
+        }
+
+
+        protected ComboBox newComboBox<TReturn,TComboItem>(Expression<Func<TEntity,TReturn>> item, string caption,List<TComboItem> comboList,Expression<Func<TComboItem,string>> comboTitle,Expression<Func<TComboItem,TReturn>> comboItem)
+        {
+            Label label = new Label();
+            label.Text = caption;
+
+            ComboBox comboBox = new ComboBox();
+
+
+
+            return null;
+        }
 
 
     }

b09918a27819b39b8c88c9de8bcc6885175813ee
Saeed MJ <treegp@gmail.com>
2022-08-08 12:55:36 +0430

    Add Corporation List on ComboBox to form

--- Parsys.WinClient/Views/InventoryForms/Editor.cs
+++ Parsys.WinClient/Views/InventoryForms/Editor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Parsys.DataLayer.Entities.EntityMethods;
 using Parsys.DataLayer.Entities.EntityModels;
 using Parsys.WinClient.Views.Framework;
 
@@ -10,6 +11,8 @@ namespace Parsys.WinClient.Views.InventoryForms
 {
     public class Editor : EntityEditorClass<Inventories>
     {
+        private DataLayer.Connections.ProviderMethods.ConnectToSQL con = new DataLayer.Connections.ProviderMethods.ConnectToSQL();
+        private CorporationsRepository corpRepo;
 
         public Editor()
         {
@@ -17,12 +20,14 @@ namespace Parsys.WinClient.Views.InventoryForms
             Id = "EditInventories";
             MultipleInstance = false;
             Title = "";
+            corpRepo = new CorporationsRepository(con.GetConnection());
+
         }
 
         protected override void OnLoad(EventArgs e)
         {
             newTextBox(entity => entity.Title, "عنوان انبار");
-
+            newComboBox(entity => entity.CorporationId, "شرکت/سازمان", corpRepo.GetAll(), s => s.Title, i => i.Id);
             newTextBox(entity => entity.Description, "توضیحات");
 
             base.OnLoad(e);

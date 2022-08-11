9e78b227114a244faa85ff8f52eee7f078a45de9
Saeed MJ <treegp@gmail.com>
2022-08-11 17:58:33 +0430

    initiate datePicker Control

---
+++ Parsys.WinClient/Views/Framework/DatePickerControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parsys.WinClient.Views.Framework
+{
+    public partial class DatePickerControl : UserControl
+    {
+        public DatePickerControl()
+        {
+            InitializeComponent();
+        }
+    }
+}

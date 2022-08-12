2cc2731ae0330ec1efe3ccca427d68ac43557c7c
Saeed MJ <treegp@gmail.com>
2022-08-12 09:39:17 +0430

    override Load event

--- Parsys.WinClient/Views/Framework/DatePickerControl.cs
+++ Parsys.WinClient/Views/Framework/DatePickerControl.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,32 @@ namespace Parsys.WinClient.Views.Framework
 {
     public partial class DatePickerControl : UserControl
     {
+        public Calendar PerCalendar = new PersianCalendar();
+        public DateTime ReturnDate;
+
         public DatePickerControl()
         {
             InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            int d = PerCalendar.GetDayOfMonth(DateTime.Now);
+            int m = PerCalendar.GetMonth(DateTime.Now);
+            int y = PerCalendar.GetYear(DateTime.Now);
+
+            int dINm = PerCalendar.GetDaysInMonth(y,m);
+            int dINlastm = m == 1 ? PerCalendar.GetDaysInMonth(y-1, 12) : PerCalendar.GetDaysInMonth(y, m-1);
+            
+
+
+
+
+
+
+
+
+            base.OnLoad(e);
         }
     }
 }

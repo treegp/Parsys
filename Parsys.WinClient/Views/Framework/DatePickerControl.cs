074e1ba279652f3d1777dc50f8bfbeb186539449
Saeed MJ <treegp@gmail.com>
2022-08-12 17:31:01 +0430

    add method to align listBox manually

--- Parsys.WinClient/Views/Framework/DatePickerControl.cs
+++ Parsys.WinClient/Views/Framework/DatePickerControl.cs
@@ -13,7 +13,27 @@ namespace Parsys.WinClient.Views.Framework
 
         public DatePickerControl()
         {
+            
             InitializeComponent();
+            MonthListBox.DrawMode = DrawMode.OwnerDrawFixed;
+            MonthListBox.DrawItem += new DrawItemEventHandler(listBox_DrawItem);
+            YearListBox.DrawMode = DrawMode.OwnerDrawFixed;
+            YearListBox.DrawItem += new DrawItemEventHandler(listBox_DrawItem);
+
+            void listBox_DrawItem(object sender, DrawItemEventArgs e)
+            {
+                ListBox list = (ListBox)sender;
+                if (e.Index > -1)
+                {
+                    object item = list.Items[e.Index];
+                    e.DrawBackground();
+                    e.DrawFocusRectangle();
+                    Brush brush = new SolidBrush(e.ForeColor);
+                    SizeF size = e.Graphics.MeasureString(item.ToString(), e.Font);
+                    e.Graphics.DrawString(item.ToString(), e.Font, brush, e.Bounds.Left + (e.Bounds.Width / 2 - size.Width / 2), e.Bounds.Top + (e.Bounds.Height / 2 - size.Height / 2));
+                }
+            }
+
         }
 
         protected override void OnLoad(EventArgs e)
@@ -95,6 +115,30 @@ namespace Parsys.WinClient.Views.Framework
         {
             if (DaysDataGridView.SelectedCells[0].Tag != null)
                 SelectedDateLabel.Text = ((DateTime)DaysDataGridView.SelectedCells[0].Tag).ToString("d MMM yyyy");
+        }
+
+        private void MonthLabel_Click(object sender, EventArgs e)
+        {
+            MonthListBox.Visible = true;
+            MonthListBox.SelectedItem = MonthLabel.Text;
+        }
+
+        private void MonthListBox_Click(object sender, EventArgs e)
+        {
+            MonthListBox.Visible = false;
+            MonthLabel.Text = MonthListBox.SelectedItem.ToString();
+        }
+
+        private void YearLabel_Click(object sender, EventArgs e)
+        {
+            YearListBox.Visible = true;
+            YearListBox.SelectedItem = YearLabel.Text;
+        }
+
+        private void YearListBox_Click(object sender, EventArgs e)
+        {
+            YearListBox.Visible = false;
+            YearLabel.Text = YearListBox.SelectedItem.ToString();
         }
     }
 }

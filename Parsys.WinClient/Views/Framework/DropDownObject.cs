df8c1061014351dbcc8be14bd45eec2c7f78a9e5
Saeed MJ <treegp@gmail.com>
2022-08-13 20:58:41 +0430

    add control to dropdownobject

--- Parsys.WinClient/Views/Framework/DropDownObject.cs
+++ Parsys.WinClient/Views/Framework/DropDownObject.cs
@@ -1,34 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using StructureMap.Graph;
 
 namespace Parsys.WinClient.Views.Framework
 {
     public partial class DropDownObject : UserControl
     {
+        public Form Form;
+        public Func<Control> ControlObject;
+
         public DropDownObject()
         {
             InitializeComponent();
+            Form = new Form();
+            Form.FormBorderStyle = FormBorderStyle.None;
+            Form.BackColor = Color.Gainsboro;
+            Form.Width = this.Width;
+            Form.Height = this.Width;
+            Form.ShowInTaskbar = false;
+            Form.TopMost = true;
+
+
+
+
+
         }
 
         private void DropDownButton_Click(object sender, EventArgs e)
         {
-            var form = new Form();
-            form.FormBorderStyle = FormBorderStyle.None;
-            form.BackColor = Color.Gainsboro;
-            form.Width = 400;
-            form.Height = 400;
-            form.ShowInTaskbar = false;
-            form.TopMost = true;
+            var locationInScreen = this.PointToScreen(Point.Empty);
 
-            form.Show();
 
+            if (Form.Visible)
+                Form.Hide();
+            else
+                Form.Show();
+
+
+
+
+
+            Form.Location = locationInScreen;
+            Form.Top += this.Height;
 
 
         }

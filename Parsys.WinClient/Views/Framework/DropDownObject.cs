e8a938311e4a82448ad9acb07c53e6891a6d39ab
Saeed MJ <treegp@gmail.com>
2022-08-13 20:15:29 +0430

    Add new control

---
+++ Parsys.WinClient/Views/Framework/DropDownObject.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parsys.WinClient.Views.Framework
+{
+    public partial class DropDownObject : UserControl
+    {
+        public DropDownObject()
+        {
+            InitializeComponent();
+        }
+
+        private void DropDownButton_Click(object sender, EventArgs e)
+        {
+            var form = new Form();
+            form.FormBorderStyle = FormBorderStyle.None;
+            form.BackColor = Color.Gainsboro;
+            form.Width = 400;
+            form.Height = 400;
+            form.ShowInTaskbar = false;
+            form.TopMost = true;
+
+            form.Show();
+
+
+
+        }
+    }
+}

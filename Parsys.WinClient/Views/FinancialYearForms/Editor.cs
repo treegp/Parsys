f0b5d8d24be09dd0048c38d675a762f148a8720f
Saeed MJ <treegp@gmail.com>
2022-08-15 14:07:03 +0430

    Add Editor for FinancialYears

---
+++ Parsys.WinClient/Views/FinancialYearForms/Editor.cs
@@ -0,0 +1,35 @@
+﻿using Parsys.DataLayer.Entities.EntityMethods;
+using Parsys.DataLayer.Entities.EntityModels;
+using Parsys.WinClient.Views.Framework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parsys.WinClient.Views.FinancialYearForms
+{
+    public partial class Editor : EntityEditorClass<FinancialYears>
+    {
+        private DataLayer.Connections.ProviderMethods.ConnectToSQL con = new DataLayer.Connections.ProviderMethods.ConnectToSQL();
+        private FinancialYearsRepository yearRepo;
+        private CorporationsRepository corpRepo;
+
+        public Editor()
+        {
+            InitializeComponent();
+            Id = "EditFinancialYears";
+            Title = "";
+            MultipleInstance = false;
+
+
+            yearRepo = new FinancialYearsRepository(con.GetConnection());
+            corpRepo = new CorporationsRepository(con.GetConnection());
+
+        }
+    }
+}

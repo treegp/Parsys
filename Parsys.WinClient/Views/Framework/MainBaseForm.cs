98f8b7ac5fcb561c7b13c58c9026f970ff58ef65
Saeed MJ <treegp@gmail.com>
2022-08-18 17:52:51 +0430

    Update Base

--- Parsys.WinClient/Views/Framework/MainBaseForm.cs
+++ Parsys.WinClient/Views/Framework/MainBaseForm.cs
@@ -16,43 +16,7 @@ namespace Parsys.WinClient.Views.Framework
             InitializeComponent();
             menuManager = new MenuStripHandler(MainMenuStrip.Items);
             viewManager = new ViewHandler(MainTabControl);
-
-            var p = new System.Globalization.PersianCalendar();
-            //MainTabPage.Controls.Add(new DatePickerControl(new DateTime(1354,4,11, p)));
-
-            var drp = new DropDownObject();
-            MainTabPage.Controls.Add(drp);
-            drp.ReturnMask = "0000/00/00";
-            drp.RightToLeft = RightToLeft.No;
-
-            drp.ControlObject = () =>
-            {
-                var datePicker = new DatePickerControl();
-
-                try
-                {
-                    if (int.Parse(drp.ReturnString.Substring(0, 4)) > 1330 & int.Parse(drp.ReturnString.Substring(0, 4)) < 1430)
-
-                        datePicker.ReturnDate = new DateTime(int.Parse(drp.ReturnString.Substring(0, 4)), int.Parse(drp.ReturnString.Substring(5, 2)), int.Parse(drp.ReturnString.Substring(8, 2)), new System.Globalization.PersianCalendar());
-                    else
-                        datePicker.ReturnDate = (DateTime)drp.ReturnObject;
-                }
-                catch
-                {
-                    if (drp.ReturnObject != null)
-                        datePicker.ReturnDate = (DateTime)drp.ReturnObject;
-                }
-
-
-                datePicker.OnChoose += (obj,e) =>
-                {
-                    drp.ReturnString = datePicker.ReturnDate.ToString("yyyyMMdd");
-                    drp.ReturnObject = datePicker.ReturnDate;
-                    drp.CloseDropDown();
-                };
-               
-                return datePicker;
-            };
+            
 
 
             var dt = MainStatusStrip.Items.Add(DateTime.Now.ToString("dd MMM yyyy  |  HH:mm:ss"));

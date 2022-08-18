03274702a0fc7d439914118093f885324517f5e4
Saeed MJ <treegp@gmail.com>
2022-08-18 15:28:38 +0430

    Set OnDoubleClick event

--- Parsys.WinClient/Views/FinancialYearForms/List.cs
+++ Parsys.WinClient/Views/FinancialYearForms/List.cs
@@ -25,6 +25,7 @@ namespace Parsys.WinClient.Views.FinancialYearForms
             corpRepo = new CorporationsRepository(con.GetConnection());
 
 
+
             AddButtun("جدید", b =>
             {
                 var form = ViewManagement.ShowForm<Editor>(frm => frm.Title = "تعریف سال مالی جدید", true);
@@ -36,7 +37,10 @@ namespace Parsys.WinClient.Views.FinancialYearForms
                 }
             });
 
-            AddButtun("ویرایش", b =>
+            AddButtun("ویرایش", b => { EditButton(); });
+
+
+            AddButtun("حذف", b =>
             {
                 if (grid.CurrentItem == null)
                 {
@@ -44,40 +48,53 @@ namespace Parsys.WinClient.Views.FinancialYearForms
                     return;
                 }
 
-                var form = ViewManagement.ShowForm<Editor>(frm=>
+                if (MessageBox.Show("آیا قصد حذف " + grid.CurrentItem.Title + " را دارید", "حذف",
+                        MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
                 {
-                    frm.Title = "ویرایش سال مالی";
-                    frm.Entity = grid.CurrentItem;
-                },true);
-
-                var t = new FinancialYears();
-                if (((Form)form.Parent).DialogResult == DialogResult.OK)
-                {
-                    t = yearRepo.Update(form.Entity);
-                    grid.UpdateItem(t);
-
+                    yearRepo.Delete(grid.CurrentItem);
+                    grid.RefreshDataSource(yearRepo.GetAll());
                 }
-
-
-
             });
 
 
-            AddButtun("حذف", b =>
-            {
 
-            });
         }
+
+        public void EditButton()
+        {
+            if (grid.CurrentItem == null)
+            {
+                MessageBox.Show("سطری انتخاب نشده است", "خطا", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var form = ViewManagement.ShowForm<Editor>(frm =>
+            {
+                frm.Title = "ویرایش سال مالی";
+                frm.Entity = grid.CurrentItem;
+            }, true);
+
+            var t = new FinancialYears();
+            if (((Form)form.Parent).DialogResult == DialogResult.OK)
+            {
+                t = yearRepo.Update(form.Entity);
+                grid.UpdateItem(t);
+            }
+        }
+
 
         protected override void OnLoad(EventArgs e)
         {
             grid = new GridHandler<FinancialYears>(this, yearRepo.GetAll());
+            grid.OnDoubleClick += (o, a) => { EditButton(); };
 
             grid.AddStringColumn(c => c.Id, "شناسه");
             grid.AddStringColumn(c => corpRepo.GetById(c.CorporationId), "عنوان شرکت");
             grid.AddStringColumn(c => c.Title, "عنوان سال مالی");
             grid.AddStringColumn(c => c.StartDate, "تاریخ افتتاح");
             grid.AddStringColumn(c => c.FinishDate, "تاریخ پایان");
+
+
             base.OnLoad(e);
         }
     }

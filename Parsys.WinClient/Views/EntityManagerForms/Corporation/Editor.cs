9f7cced09fbc8dd03b54179f2e98ea83efb28c6b
Saeed MJ <treegp@gmail.com>
2022-08-18 23:44:11 +0430

    initialize Editor For Corporation Model

---
+++ Parsys.WinClient/Views/EntityManagerForms/Corporation/Editor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Parsys.DataLayer.Entities.EntityModels;
+using Parsys.WinClient.Views.Framework;
+
+namespace Parsys.WinClient.Views.EntityManagerForms.Corporation
+{
+    public partial class Editor : EntityEditorClass<Corporations>
+    {
+
+
+        public Editor()
+        {
+            InitializeComponent();
+        }
+
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+
+
+        }
+    }
+}

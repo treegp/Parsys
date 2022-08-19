7250763418e1402c400d260432f5186de4e628f9
Saeed MJ <treegp@gmail.com>
2022-08-19 17:56:54 +0430

    fix base

--- Parsys.WinClient/Views/Framework/ViewBaseControl.Designer.cs
+++ Parsys.WinClient/Views/Framework/ViewBaseControl.Designer.cs
@@ -26,7 +26,7 @@
         /// Required method for Designer support - do not modify 
         /// the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent()
+        protected void InitializeComponent()
         {
             this.ButtonsPanel = new System.Windows.Forms.Panel();
             this.SuspendLayout();

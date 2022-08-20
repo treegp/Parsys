2fb55f3a3c04da1ae451feb81792df3c0740d0da
Saeed MJ <treegp@gmail.com>
2022-08-20 21:22:08 +0430

    change list codes

--- Parsys.WinClient/Views/EntityManagerForms/ProductCategories/List.cs
+++ Parsys.WinClient/Views/EntityManagerForms/ProductCategories/List.cs
@@ -2,6 +2,7 @@
 using Parsys.DataLayer.Entities.EntityMethods;
 using Parsys.WinClient.Views.Framework;
 using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace Parsys.WinClient.Views.EntityManagerForms.ProductCategories
@@ -21,74 +22,9 @@ namespace Parsys.WinClient.Views.EntityManagerForms.ProductCategories
             repo = new ProductCategoriesRepository(con.GetConnection());
             invRepo = new InventoriesRepository(con.GetConnection());
 
-            grid = new GridHandler<DataLayer.Entities.EntityModels.ProductCategories>(this, repo.GetByIsDeleted(false));
-
             Title = "لیست دسته بندی ها";
             Id = "ProductCategoriesList";
             MultipleInstance = false;
-
-            AddButtun("جدید", b =>
-            {
-                var newForm = ViewManagement.ShowForm<Editor>((nw) =>
-                {
-                    nw.Entity = new DataLayer.Entities.EntityModels.ProductCategories();
-                    nw.Title = "تعریف دسته بندی جدید";
-                }, true);
-
-                if (((Form)newForm.Parent).DialogResult == DialogResult.OK)
-                {
-                    repo.Insert(newForm.Entity);
-                    grid.AddItem(newForm.Entity);
-                }
-
-
-            });
-
-            AddButtun("ویرایش", b =>
-            {
-                onDoubleClick();
-            });
-
-            AddButtun("حذف", b =>
-            {
-                if (grid.CurrentIndex == -1)
-                {
-                    MessageBox.Show("سطری انتخاب نشده است", "پیام سیستم", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    return;
-                }
-
-                if (MessageBox.Show("آیا از حذف \"" + grid.CurrentItem.Title + "\" اطمینان دارید", "پیام سیستم", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
-                {
-                    grid.CurrentItem.IsDeleted = true;
-                    grid.CurrentItem.DeleteDate = DateTime.Now;
-                    repo.Update(grid.CurrentItem);
-                    grid.RemoveItem(grid.CurrentIndex);
-                }
-
-
-            });
-        }
-
-        protected void onDoubleClick()
-        {
-            if (grid.CurrentIndex == -1)
-            {
-                MessageBox.Show("سطری انتخاب نشده است", "پیام سیستم", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
-
-            var edtView = ViewManagement.ShowForm<Editor>(edt =>
-            {
-                edt.Entity = grid.CurrentItem;
-                edt.Title = "ویرایش دسته بندی";
-            }, true);
-
-
-            if (((Form)edtView.Parent).DialogResult == DialogResult.OK)
-            {
-                repo.Update(edtView.Entity);
-                grid.UpdateItem(edtView.Entity, grid.CurrentIndex);
-            }
 
         }
 
@@ -96,17 +32,40 @@ namespace Parsys.WinClient.Views.EntityManagerForms.ProductCategories
         protected override void OnLoad(EventArgs e)
         {
             base.OnLoad(e);
-
-            grid.OnDoubleClick += (s, a) => { onDoubleClick(); };
-
-
-            grid.AddStringColumn(i => i.Title, "عنوان");
-            grid.AddStringColumn(i => i.InventoryId, "انبار");
-            grid.AddStringColumn(i => i.ParentCategoryId, "دسته بندی والد");
-            grid.AddStringColumn(i => i.Description, "توضیحات");
+            TreeControl treeControl = new TreeControl(this);
+            treeControl.OnExpandTree += (parentNode, parentObject) =>
+            {
+                List<TreeItem> roots = new List<TreeItem>();
+                if (parentNode == null)
+                {
 
 
+                    for (int i = 1; i <10; i++)
+                    {
+                        Items p = new Items() { Id= 20 + i, Name = "Item " + i };
+
+                        roots.Add(new TreeItem()
+                        {
+                            Text = p.Name,
+                            Object = p
+                        });
+                    }
+
+                }
+
+                return roots;
+            };
+
+            treeControl.InitializeTree();
 
         }
+    }
+
+
+    public class Items
+    {
+        
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }

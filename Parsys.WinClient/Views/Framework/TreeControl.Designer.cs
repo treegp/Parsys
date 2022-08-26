1fb9d8bffd6e9fb85589d9f26c0fff7a27bfeda3
Saeed MJ <treegp@gmail.com>
2022-08-26 15:22:10 +0430

    Update Treecontrol

--- Parsys.WinClient/Views/Framework/TreeControl.Designer.cs
+++ Parsys.WinClient/Views/Framework/TreeControl.Designer.cs
@@ -28,8 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // TreeControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "TreeControl";
+            this.Size = new System.Drawing.Size(313, 254);
+            this.ResumeLayout(false);
+
         }
 
         #endregion

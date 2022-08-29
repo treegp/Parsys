b73bd1872808fa43006144f2b65a2d041d6fffbd
Saeed MJ <treegp@gmail.com>
2022-08-29 12:58:15 +0430

    fix problems

--- Parsys.WinClient/Views/Framework/ViewBaseControl.cs
+++ Parsys.WinClient/Views/Framework/ViewBaseControl.cs
@@ -5,38 +5,52 @@ namespace Parsys.WinClient.Views.Framework
 {
     public partial class ViewBaseControl : UserControl
     {
-
         public string Title { get; set; }
         public string Id { get; set; }
         public bool MultipleInstance { get; set; } = false;
         public ViewHandler ViewManagement { get; internal set; }
+        public Panel ButtonsPanel;
 
 
-        protected void CloseThis(DialogResult? result=null)
+        protected void CloseThis(DialogResult? result = null)
         {
-            ViewManagement.CloseView(this,result);
+            ViewManagement.CloseView(this, result);
         }
 
 
-        protected Button AddButtun(string title,Action<Button> onPress)
+        protected Button AddButtun(string title, Action<Button> onPress)
         {
-            ButtonsPanel.Visible=true;
+            ButtonsPanel.Visible = true;
             int bottunsCount = ButtonsPanel.Controls.Count;
             int left = 20 + (90 + 5) * bottunsCount;
             var btn = new Button();
             btn.Text = title;
-            btn.Click += (o,e) => onPress(btn);
+            btn.Click += (o, e) => onPress(btn);
             btn.Size = new System.Drawing.Size(90, 25);
             btn.Location = new System.Drawing.Point(left, 6);
+
+            if (ButtonsPanel.Dock != DockStyle.Bottom)
+                this.Controls.Add(ButtonsPanel);
+
+            ButtonsPanel.Dock = DockStyle.Bottom;
             ButtonsPanel.Controls.Add(btn);
             return btn;
         }
 
         public ViewBaseControl()
         {
+
             InitializeComponent();
+            ButtonsPanel = new Panel();
+
+
+            ButtonsPanel.Height = 39;
+            ButtonsPanel.BackColor = System.Drawing.Color.Gainsboro;
+            ButtonsPanel.BorderStyle = BorderStyle.FixedSingle;
+            ButtonsPanel.Visible = false;
+
         }
 
-      
+
     }
 }

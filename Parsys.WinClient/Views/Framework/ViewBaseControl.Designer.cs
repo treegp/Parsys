4a878cfbe5b9bcb7e4445624303aec85edf67f23
Saeed MJ <treegp@gmail.com>
2022-08-29 12:58:40 +0430

    update project with file and folders

--- Parsys.WinClient/Views/Framework/ViewBaseControl.Designer.cs
+++ Parsys.WinClient/Views/Framework/ViewBaseControl.Designer.cs
@@ -28,26 +28,16 @@
         /// </summary>
         protected void InitializeComponent()
         {
-            this.ButtonsPanel = new System.Windows.Forms.Panel();
+
             this.SuspendLayout();
-            // 
-            // ButtonsPanel
-            // 
-            this.ButtonsPanel.BackColor = System.Drawing.Color.Gainsboro;
-            this.ButtonsPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.ButtonsPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.ButtonsPanel.Location = new System.Drawing.Point(0, 353);
-            this.ButtonsPanel.Margin = new System.Windows.Forms.Padding(2);
-            this.ButtonsPanel.Name = "ButtonsPanel";
-            this.ButtonsPanel.Size = new System.Drawing.Size(539, 39);
-            this.ButtonsPanel.TabIndex = 0;
-            this.ButtonsPanel.Visible = false;
+ 
+
             // 
             // ViewBaseControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.ButtonsPanel);
+
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "ViewBaseControl";
             this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
@@ -58,6 +48,6 @@
 
         #endregion
 
-        private System.Windows.Forms.Panel ButtonsPanel;
+
     }
 }
